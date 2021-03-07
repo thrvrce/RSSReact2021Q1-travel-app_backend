7f6697da3038bd7937f04beb7388a79decbce556
Victor Avdeev <avdeevvictor95@gmail.com>
2021-03-07 20:27:52 +0300

    feat: create server

---
+++ src/app.ts
@@ -0,0 +1,43 @@
+import express from "express";
+import cors from "cors";
+import aa from "./sdfsd";
+
+function normalizePort(val: any) {
+  const port = parseInt(val, 10);
+  if (Number.isNaN(port)) {
+    return val;
+  }
+
+  if (port >= 0) {
+    return port;
+  }
+
+  return false;
+}
+
+const PORT: number | string | boolean = normalizePort(process.env.port || 3000);
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use((req, res) => {
+  res.status(404).json({
+    statuscode: 404,
+    message: "Page not found",
+  });
+});
+
+app.use((err: any, req: any, res: any) => {
+  res.status(500).json({
+    statuscode: 500,
+    message: err.message,
+    stack: err.stack,
+  });
+});
+
+app.set("port", PORT);
+app.listen(PORT, () => {
+  console.log(`current port ${app.get("port")}`);
+});
